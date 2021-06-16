7b05f7c31c1e05796a15cedb7b3aaab161fc6731
Alexander Eck <eck.alexander@googlemail.com>
2021-06-16 18:23:09 +0200

    Add button on NotificationActivity screen to get back to main menu

--- app/src/main/java/com/logicloop/notifyapp/MainActivity.java
+++ app/src/main/java/com/logicloop/notifyapp/MainActivity.java
@@ -44,120 +44,114 @@ public class MainActivity extends AppCompatActivity {
         notificationManager.createNotificationChannel(notificationChannel);
 
         // Set up first notification button
-        findViewById(R.id.btn_ntfcn1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Set up main intent for notification (action when user taps notification)
-                Intent intent =
-                        new Intent(MainActivity.this, NotificationActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                PendingIntent pendingIntent = PendingIntent.getActivity(
-                        MainActivity.this,
-                        0,
-                        intent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
+        findViewById(R.id.btn_ntfcn1).setOnClickListener(v -> {
+            // Set up main intent for notification (action when user taps notification)
+            Intent intent =
+                    new Intent(v.getContext(), NotificationActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(
+                    v.getContext(),
+                    0,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
 
-                ////////////////////////////////////////////////////////////////////////////
-                ////////////////////////////////////////////////////////////////////////////
-                /////////////////////////// BUILDER PATTERN DEMO ///////////////////////////
-                // BUTTON 1: Configure and create notification
-                Notification notification =
-                        new Notification.Builder(
-                                MainActivity.this, getString(R.string.order_channel_id))
-                        .setContentIntent(pendingIntent)
-                        .setAutoCancel(true)
-                        .setSmallIcon(R.drawable.ic_baseline_message_24)
-                        .setContentTitle(getString(R.string.ntfcn1_title))
-                        .setContentText(getString(R.string.ntfcn1_text))
+            ////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////
+            /////////////////////////// BUILDER PATTERN DEMO ///////////////////////////
+            // BUTTON 1: Configure and create notification
+            Notification notification =
+                    new Notification.Builder(
+                            v.getContext(), getString(R.string.order_channel_id))
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.drawable.ic_baseline_message_24)
+                    .setContentTitle(getString(R.string.ntfcn1_title))
+                    .setContentText(getString(R.string.ntfcn1_text))
 
-                        .build();
-                /////////////////////////// BUILDER PATTERN DEMO ///////////////////////////
-                ////////////////////////////////////////////////////////////////////////////
-                ////////////////////////////////////////////////////////////////////////////
+                    .build();
+            /////////////////////////// BUILDER PATTERN DEMO ///////////////////////////
+            ////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////
 
-                // Send notification to OS
-                notificationManager.notify(NOTIFICATION1_ID, notification);
-            }
+            // Send notification to OS
+            notificationManager.notify(NOTIFICATION1_ID, notification);
         });
 
         // Set up second notification button
-        findViewById(R.id.btn_ntfcn2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Set up main intent for notification (action when user taps notification)
-                Intent intent =
-                        new Intent(MainActivity.this, NotificationActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                PendingIntent pendingIntent = PendingIntent.getActivity(
-                        MainActivity.this,
-                        0,
-                        intent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
+        findViewById(R.id.btn_ntfcn2).setOnClickListener(v -> {
+            // Set up main intent for notification (action when user taps notification)
+            Intent intent =
+                    new Intent(v.getContext(), NotificationActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(
+                    v.getContext(),
+                    0,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
 
-                // Set up secondary intent - DISMISS
-                Intent dismissIntent =
-                        new Intent(MainActivity.this, NotifyIntentService.class);
-                dismissIntent.setAction(NotifyIntentService.ACTION_DISMISS);
-                PendingIntent dismissPendingIntent = PendingIntent.getService(
-                        MainActivity.this,
-                        0,
-                        dismissIntent,
-                        0
-                );
-                Notification.Action dismissAction = new Notification.Action.Builder(
-                        Icon.createWithResource(
-                                MainActivity.this, R.drawable.ic_baseline_clear_24),
-                        getString(R.string.ntfcn2_action_dismiss), dismissPendingIntent)
-                        .build();
+            // Set up secondary intent - DISMISS
+            Intent dismissIntent =
+                    new Intent(v.getContext(), NotifyIntentService.class);
+            dismissIntent.setAction(NotifyIntentService.ACTION_DISMISS);
+            PendingIntent dismissPendingIntent = PendingIntent.getService(
+                    v.getContext(),
+                    0,
+                    dismissIntent,
+                    0
+            );
+            Notification.Action dismissAction = new Notification.Action.Builder(
+                    Icon.createWithResource(
+                            v.getContext(), R.drawable.ic_baseline_clear_24),
+                    getString(R.string.ntfcn2_action_dismiss), dismissPendingIntent)
+                    .build();
 
-                // Set up secondary intent - SNOOZE
-                Intent snoozeIntent =
-                        new Intent(MainActivity.this, NotifyIntentService.class);
-                snoozeIntent.setAction(NotifyIntentService.ACTION_SNOOZE);
-                PendingIntent snoozePendingIntent = PendingIntent.getService(
-                        MainActivity.this,
-                        0,
-                        snoozeIntent,
-                        0
-                );
-                Notification.Action snoozeAction = new Notification.Action.Builder(
-                        Icon.createWithResource(
-                                MainActivity.this, R.drawable.ic_baseline_snooze_24),
-                        getString(R.string.ntfcn2_action_snooze), snoozePendingIntent)
-                        .build();
+            // Set up secondary intent - SNOOZE
+            Intent snoozeIntent =
+                    new Intent(v.getContext(), NotifyIntentService.class);
+            snoozeIntent.setAction(NotifyIntentService.ACTION_SNOOZE);
+            PendingIntent snoozePendingIntent = PendingIntent.getService(
+                    v.getContext(),
+                    0,
+                    snoozeIntent,
+                    0
+            );
+            Notification.Action snoozeAction = new Notification.Action.Builder(
+                    Icon.createWithResource(
+                            v.getContext(), R.drawable.ic_baseline_snooze_24),
+                    getString(R.string.ntfcn2_action_snooze), snoozePendingIntent)
+                    .build();
 
-                ////////////////////////////////////////////////////////////////////////////
-                ////////////////////////////////////////////////////////////////////////////
-                /////////////////////////// BUILDER PATTERN DEMO ///////////////////////////
-                // BUTTON 2: Configure and create notification
-                Notification notification2 =
-                        new Notification.Builder(
-                                MainActivity.this, getString(R.string.order_channel_id))
-                                .setContentIntent(pendingIntent)
-                                .setAutoCancel(true)
-                                .setSmallIcon(R.drawable.ic_baseline_alarm_24)
-                                .setLargeIcon(Icon.createWithResource(
-                                        MainActivity.this, R.drawable.ic_stat_name)
-                                        .setTint(getColor(R.color.secondary_500)))
-                                .setColor(getColor(R.color.secondary_500))
-                                .setShowWhen(true)
-                                .setSubText(getString(R.string.ntfcn2_sub_text))
-                                .setContentTitle(getString(R.string.ntfcn2_title))
-                                .setContentText(getString(R.string.ntfcn2_text))
-                                .setStyle(new Notification.BigTextStyle().
-                                        bigText(getString(R.string.ntfcn2_big_text)))
-                                .setActions(dismissAction, snoozeAction)
+            ////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////
+            /////////////////////////// BUILDER PATTERN DEMO ///////////////////////////
+            // BUTTON 2: Configure and create notification
+            Notification notification2 =
+                    new Notification.Builder(
+                            v.getContext(), getString(R.string.order_channel_id))
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true)
+                            .setSmallIcon(R.drawable.ic_baseline_alarm_24)
+                            .setLargeIcon(Icon.createWithResource(
+                                    v.getContext(), R.drawable.ic_stat_name)
+                                    .setTint(getColor(R.color.secondary_500)))
+                            .setColor(getColor(R.color.secondary_500))
+                            .setShowWhen(true)
+                            .setSubText(getString(R.string.ntfcn2_sub_text))
+                            .setContentTitle(getString(R.string.ntfcn2_title))
+                            .setContentText(getString(R.string.ntfcn2_text))
+                            .setStyle(new Notification.BigTextStyle().
+                                    bigText(getString(R.string.ntfcn2_big_text)))
+                            .setActions(dismissAction, snoozeAction)
 
-                                .build();
-                /////////////////////////// BUILDER PATTERN DEMO ///////////////////////////
-                ////////////////////////////////////////////////////////////////////////////
-                ////////////////////////////////////////////////////////////////////////////
+                            .build();
+            /////////////////////////// BUILDER PATTERN DEMO ///////////////////////////
+            ////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////
 
-                // Send notification to OS
-                notificationManager.notify(NOTIFICATION2_ID, notification2);
-            }
+            // Send notification to OS
+            notificationManager.notify(NOTIFICATION2_ID, notification2);
         });
     }
 }
--- app/src/main/java/com/logicloop/notifyapp/NotificationActivity.java
+++ app/src/main/java/com/logicloop/notifyapp/NotificationActivity.java
@@ -1,8 +1,13 @@
 package com.logicloop.notifyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.TaskStackBuilder;
+import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NotificationActivity extends AppCompatActivity {
@@ -12,8 +17,17 @@ public class NotificationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
 
-        TextView textView = findViewById(R.id.textViewNotification);
-        textView.setText("We will surely implement some clever function " +
-                "- for now, tap the 'Back' button :)");
+        TextView textView = findViewById(R.id.text_view_ntfcn);
+        textView.setText(getString(R.string.text_view_ntfcn_text));
+
+        Button button = findViewById(R.id.btn_back);
+        button.setOnClickListener(v -> {
+            Intent intent =
+                    new Intent(v.getContext(), MainActivity.class);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(v.getContext());
+            stackBuilder.addParentStack(MainActivity.class);
+            stackBuilder.addNextIntent(intent);
+            startActivity(intent);
+        });
     }
 }
